f0c603d1d9e88081fa9c02e005c3379a2e05cec0
Daniel Felipe Chávez González <danielf.chavezg@konradlorenz.edu.co>
2024-11-22 00:10:25 -0500

    Integración cambios

--- FatalSecrets/FatalSecrets/src/co/edu/konradlorenz/controler/AplMain.java
+++ FatalSecrets/FatalSecrets/src/co/edu/konradlorenz/controler/AplMain.java
@@ -1,7 +1,6 @@
 package co.edu.konradlorenz.controler;
 
 public class AplMain {
-//AplMain
 	public static void main(String[] args) {
 		GameController objGameController = new GameController();
 		objGameController.run();
