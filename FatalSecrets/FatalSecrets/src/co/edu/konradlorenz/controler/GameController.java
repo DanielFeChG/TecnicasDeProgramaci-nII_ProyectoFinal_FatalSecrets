4448c556d25a82e97c6485487718bc89a4f3dad7
Daniel Felipe Chávez González <danielf.chavezg@konradlorenz.edu.co>
2024-11-26 13:41:35 -0500

    Riddles update
    Se agregaron todos los diálogos de los acertijos de los NPCs

--- FatalSecrets/FatalSecrets/src/co/edu/konradlorenz/controler/GameController.java
+++ FatalSecrets/FatalSecrets/src/co/edu/konradlorenz/controler/GameController.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
@@ -97,15 +99,51 @@ public class GameController implements ActionListener{
 		
 		// ------------------------------------ CREAR ACERTIJOS ------------------------------------
 		
+		Random rd = new Random();
+		
 		//Todos los acertijos se deben recibir en minúsculas y sin signos de puntuacion
-		Riddle act1Estudiante = new Riddle("No recuerdo mi código estudiantil", "506232729");
-		Riddle act2Conserje = new Riddle("No recuerdo dóde dejé el trapero", "pasillo 7 piso");
-		Riddle act3Instructora = new Riddle("Por asignar...", "solucion");//Con las adivinqanzas de las aulas virtuales
-		Riddle act4Bibliotecario = new Riddle("Necesito ubicar este libro en la sección correcta", "psicologia");
-		Riddle act5Profesora = new Riddle("Necesito encontrar el número de la llave de este salón", "144");
-		Riddle act6Recepcionista = new Riddle("Yo creo mucho en la suerte, solo hablaré contigo si adivinas el número en el que estoy pensando", "0");//Este numero es Random
-		Riddle act7Cocinero = new Riddle("Necesito que ajustes el tiempo del microondas", "");//Tiempo real
-		Riddle act8Vigilante = new Riddle("Necesito recordar la clave de acceso al sistema de seguridad de la universidad", "fukonradl");	
+		Riddle act1Estudiante = new Riddle("Tú - Buenas tardes joven, ¿me permite hacerle unas preguntas?\r\n"
+				+ "Estudiante – Por el momento estoy muy ocupado y además estoy estresado porque estoy tratando de entregar una tarea de técnicas de programación II, pero no recuerdo mi código estudiantil.\r\n"
+				+ "Tú - Pero… ¿El código no se encuentra en su carné estudiantil?\r\n"
+				+ "Estudiante – Si, pero justo hoy se me perdió, en los tres semestres que llevo en la universidad nunca me había sucedido esto.\r\n"
+				+ "Tú - Trataré de buscar su carné y cuando lo encuentre le recordaré su código. \r\n"
+				+ "", "506232729");
+		Riddle act2Conserje = new Riddle("Tú - Buenas tardes, ¿se encuentra muy ocupado? ¿me regala un momento para hacerle unas preguntas?\r\n"
+				+ "Conserje - Buenas tardes, sí me encuentro ocupado, debo terminar de hacer la limpieza en otros dos pisos para terminar mi turno y estoy un poco retrasado porque no recuerdo dónde dejé el trapero.\r\n"
+				+ "Tú - ¿Si lo ayudo a encontrarlo podría regalarme tan solo unos minutos?\r\n"
+				+ "Conserje – Está bien.\r\n"
+				+ "", "pasillo 7 piso");
+		Riddle act3Instructora = new Riddle("Tú - Buenas tardes, soy… Mmm veo que se encuentra algo desesperada, ¿puedo ayudarla con algo?\r\n"
+				+ "Instructora - Verás, perdí la combinación del candado de mi casillero. Guardaba algunas cosas personales importantes allí, y ahora... no sé qué hacer. \r\n"
+				+ "Tú - ¿Sabe si la combinación está registrada en algún lugar?\r\n"
+				+ "Instructora - No, no está escrita en ningún lado. Pero... ahora que lo mencionas... escuché a algunos estudiantes hace unos días hablando de un código. Uno de ellos mencionó algo sobre pistas escondidas en algunos perfiles. Estoy segura de que están haciéndome una broma.\r\n"
+				+ "Tú - ¿Quiénes son esos estudiantes?\r\n"
+				+ "Instructora - Bueno, no quiero acusar a nadie sin pruebas, pero hay un grupo que suele venir bastante seguido al gimnasio: SANABRIA CAMPOS Juan, CHAVEZ GONZALEZ Daniel, AVILA ZULUAGA Esteban y GARCIA RODRIGUEZ Sebastián. Puede que ellos tengan que ver con esto porque esos estudiantes son... un tanto traviesos.\r\n"
+				+ "Tú - Muy bien, investigaré eso. \r\n"
+				+ "Instructora - Te lo agradecería mucho. Necesito abrir este casillero lo antes posible. ¡Y no tengo ni idea de qué hacer si no encuentro la combinación!\r\n"
+				+ "", "solucion");//Con las adivinanzas de las aulas virtuales
+		Riddle act4Bibliotecario = new Riddle("Tú -¿Buenas tardes, señor, que está haciendo?\r\n"
+				+ "Bibliotecario - Estoy organizando unos libros que han devuelto los estudiantes, pero no he podido encontrar la ubicación de este libro porque se le despegó el sticker que muestra dónde ubicarlo…\r\n"
+				+ "Tú - Tal vez yo pueda ayudarle…\r\n"
+				+ "", "psicologia");
+		Riddle act5Profesora = new Riddle("Tú - Disculpe señora, buenas tardes, parece que necesita ayuda. ¿Hay algo en lo que pueda colaborar?\r\n"
+				+ "Profesora – Buenas tardes, la situación es que en media hora tengo que dictar clase en este salón, pero no recuerdo el número de llave que abre justo esta puerta, si pudieras ayudarme estaría muy agradecida.\r\n"
+				+ "Tú – Entendido, veré que puedo hacer… \r\n"
+				+ "", "144");
+		Riddle act6Recepcionista = new Riddle("Tú: Buenas tardes.\r\n"
+				+ "Recepcionista: Solo atiendo a decanos y directivos.\r\n"
+				+ "Tú: Hmm…", String.valueOf(rd.nextInt(10)+1));//Este numero es Random
+		Riddle act7Cocinero = new Riddle("Tú: Buenas tardes. Disculpe, ¿qué está haciendo?\r\n"
+				+ "Cocinero: Hola. Estoy tratando de ajustar el tiempo del microondas, pero no logro configurarlo correctamente.\r\n"
+				+ "Tú: ¿Pueda ayudar?\r\n"
+				+ "Cocinero: Hola. Este microondas se desconfiguró y no he podido ajustar el tiempo correctamente. Podrías intentarlo tú, ¿verdad?\r\n"
+				+ "Tú: Claro, déjeme verlo.", "");//Tiempo real
+		Riddle act8Vigilante = new Riddle("Tú - Buenas tardes, ¿podría ayudarme con unas preguntas?\r\n"
+				+ "Vigilante - Buenas tardes, claro, pero... ahora mismo estoy lidiando con un problema.\r\n"
+				+ "Tú - ¿Algo en lo que pueda ayudarle?\r\n"
+				+ "Vigilante - La verdad, sí. Soy nueva aquí, y me asignaron el turno para revisar las cámaras de seguridad, pero no puedo acceder al sistema sin el código de acceso.\r\n"
+				+ "Tú - ¿No le dejaron el código anotado?\r\n"
+				+ "Vigilante - No, el vigilante anterior se retiró de forma apresurada y no dejó claro cómo funciona todo. ", "fukonradl");	
 		
 		// ------------------------------------ CREAR NPCS -----------------------------------------
 		
@@ -119,12 +157,12 @@ public class GameController implements ActionListener{
 				+ "", studentID, Color.RED);
 		NPC estudiante = new Innocent("Daniel", "Estudiante", "Fuí a la oficina del decano porque...", act1Estudiante, stickerBook, Color.BLUE);
 		NPC conserje = new Innocent("Fabian", "Conserje", "He estado haciendo el aseo de los baños de todos los pisos...", act2Conserje, keysGuide, Color.GREEN);
-		NPC instructura = new Innocent("Natalia", "Instructora del gimnasio", "Como todos los dias he estado unicamente en el giimnasio...", act3Instructora, null, Color.YELLOW);
+		NPC instructura = new Innocent("Natalia", "Instructora", "Como todos los dias he estado unicamente en el giimnasio...", act3Instructora, null, Color.YELLOW);
 		NPC bibliotecario = new Innocent("Uriel", "Bibliotecario", "Estaba organizando uns libros que devolvieron.. pero kmlkf sdopef eefefmrlk m kefmwkemfkewmo kefmefmwolf oef ef i efmwekmlmewflnwef ked knmlkwefm este es el final", act4Bibliotecario, null, Color.PINK);
 		NPC profesora = new Innocent("Lucía", "Profesora", "Estaba reemplazando al profesor en la clase de programación pq no llegó...", act5Profesora, null, Color.CYAN);
-		NPC recepcionista = new Innocent("Rocio", "Recepcionista de decanatura", "Todo el día he estado en mi puesto de trabajo...", act6Recepcionista, null, Color.MAGENTA);
+		NPC recepcionista = new Innocent("Rocio", "Recepcionista", "Todo el día he estado en mi puesto de trabajo...", act6Recepcionista, null, Color.MAGENTA);
 		NPC vigilante = new Innocent("Adriana", "Vigilante", "Acabo de llegar a mi turno...", act8Vigilante, null, Color.ORANGE);
-		NPC cocinero = new Murder("Oscar", "Cocinero de la terraza del sexto piso", "Hoy he estado en la caseta durante todo el día...", act7Cocinero, null, Color.BLACK);
+		NPC cocinero = new Murder("Oscar", "Cocinero", "Hoy he estado en la caseta durante todo el día...", act7Cocinero, null, Color.BLACK);
 		
 		// ------------------------------------ CREAR UBICACIONES ------------------------------------
 		
--- FatalSecrets/FatalSecrets/src/co/edu/konradlorenz/view/RiddleDialog.java
+++ FatalSecrets/FatalSecrets/src/co/edu/konradlorenz/view/RiddleDialog.java
@@ -2,6 +2,8 @@ package co.edu.konradlorenz.view;
 
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.event.ActionListener;
@@ -24,39 +26,45 @@ public class RiddleDialog extends JDialog {
 	
 	public RiddleDialog(GameWindow gameWindow, String titulo, String acertijo) {
 		setTitle("Interacción con NPC");
-        setSize(400, 300);
+        setSize(400, 350);
         setLocationRelativeTo(gameWindow);
         setModal(true);
         setResizable(false);
         getContentPane().setLayout(null);
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
-        
         lblTitulo = new JLabel(titulo);
-        lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        lblTitulo.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
         lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
-        lblTitulo.setBounds(139, 10, 98, 31);
+        lblTitulo.setBounds(90, 10, 200, 31);
         getContentPane().add(lblTitulo);
         
         respuestaField = new JTextField();
-        respuestaField.setBounds(96, 206, 146, 19);
+        respuestaField.setBounds(92, 260, 146, 19);
         getContentPane().add(respuestaField);
         respuestaField.setColumns(10);
         
         checkButton = new JButton("Comprobar");
-        checkButton.setBounds(252, 206, 98, 21);
+        checkButton.setBounds(248, 259, 98, 21);
         getContentPane().add(checkButton);
         
-        lblacertijo = new JLabel("<html><center>"+acertijo+"</html>");
-        lblacertijo.setHorizontalAlignment(SwingConstants.CENTER);
-        lblacertijo.setBounds(38, 52, 294, 143);
-        getContentPane().add(lblacertijo);
+     // Crear el JTextArea
+        JTextArea textArea = new JTextArea();
+        textArea.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        textArea.setText(acertijo);
+        textArea.setEditable(false);
+        textArea.setLineWrap(true); // Ajustar líneas automáticamente
+        textArea.setWrapStyleWord(true); // Ajustar por palabras completas
+
+        // Agregar el JTextArea dentro de un JScrollPane
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setBounds(13, 50, 360, 200); // Posición y tamaño
+        getContentPane().add(scrollPane);
         
         lblResultado = new JLabel("");
         lblResultado.setHorizontalAlignment(SwingConstants.CENTER);
-        lblResultado.setBounds(106, 236, 131, 14);
+        lblResultado.setBounds(100, 280, 131, 14);
         getContentPane().add(lblResultado);
-
 	}
 	
     public String getAnswer() {
